afe953cf2f0d7fd9bb514423984e07a32161877b
Ivan <ipkozyrin@gmail.com>
2023-06-16 11:50:55 -0300

    change

--- src/stdLib/Slot.ts
+++ src/stdLib/Slot.ts
@@ -15,66 +15,40 @@ export const SLOT_DEFAULT = 'default'
 
 class SlotComponent extends Component {
 
-  // TODO: нельзя передавать slotsDefinition - ошибка
-  // TODO: нужно взять slotDefinition из parent
-  //  и выполнить в scopedComponent.execSlot()
-
   async init() {
+    if (this.slotsDefinition) {
+      throw new Error(`You can't define slots definition to Slot component`)
+    }
+
     await super.init()
-
-
   }
 
 
   protected instantiateChildrenComponents(): Component[] {
     let cmpDefinitions: CmpInstanceDefinition[] = []
 
-    //const scope = this.scope.$newScope(vars)
-
     // TODO: как scope использовать ???
-    // TODO: наверное его подменить у компонента
-    // TODO: а если такой компонент есть, то ему подменить scope ???
-    // TODO: надо проверить есть ли такой компонент и если нет то тогда создать
-    // TODO: или получается что Slot просто их создает как своих потомков
+    //const scope = this.scope.$newScope(vars)
 
     if (!this.parent) throw new Error('No parent')
     else if (!this.parent.slotsDefinition) throw new Error('No slotsDefinition in parent')
+    else if (!this.scopeComponent) throw new Error('No scopeComponent')
 
-    const slot: CmpInstanceDefinition[] | undefined = this.parent
+    const slot = this.parent
       .slotsDefinition[this.props.name || SLOT_DEFAULT]
 
     if (!slot) throw new Error('No any slot in parent')
+
+    // TODO: надо чтобы props этого потомка выполнился с параметрами slot
 
     return cmpDefinitions
       .map((el) => instantiateChildComponent(
         el,
         this.app,
         this,
-        // all my direct children in my scope
-        this
+        this.scopeComponent!
       ))
   }
-
-  /**
-   * Execute slot of some deep child component slot.
-   * @param slotDefinitionToRender
-   * @param parent
-   * @param vars - some variables which will be put so scope where slot will be executed
-   */
-  async execSlot(
-    slotDefinitionToRender: CmpInstanceDefinition[],
-    parent: Component,
-    vars?: Record<string, any>
-  ) {
-
-    return slotDefinitionToRender.map((el) => instantiateChildComponent(
-      el,
-      this.app,
-      parent,
-      this,
-    ))
-  }
-
 }
 
 
