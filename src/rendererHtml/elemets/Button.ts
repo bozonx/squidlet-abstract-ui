ea7f6ae9445eefb4c983c1fc9a200a20bcca0e33
Ivan <ipkozyrin@gmail.com>
2023-06-23 16:52:54 -0300

    add

--- src/rendererHtml/elemets/Button.ts
+++ src/rendererHtml/elemets/Button.ts
@@ -4,9 +4,19 @@ import {COMPONENT_DATA_MARKER} from '../constants.js';
 
 export function renderButton(el: RenderedElement): string {
   const id = `${COMPONENT_DATA_MARKER}="${el.componentId}"`
-  const href = `href=${el.params?.to || ''}`
+  const disabled: string = (el.params?.disabled) ? `disabled="disabled"` : ''
 
-  // TODO: add showExternalIcon
+  // TODO: add leftIcon
 
-  return `<a ${id} ${href}>${el.params?.value || ''}</a>`
+  if (el.params?.to) {
+    const href = `href=${el.params.to}`
+
+    // TODO: add showExternalIcon
+
+    return `<a class="s-button-link" ${id} ${href}>${el.params?.value || ''}</a>`
+  }
+
+  // TODO: add pending
+
+  return `<button class="s-button" ${id} ${disabled}>${el.params?.value || ''}</button>`
 }
